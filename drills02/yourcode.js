94e74c5aa8de11bf6d315aa9c9edaba03cbbb5c1
CJ Wang <shijie.wang1990@gmail.com>
2019-03-26 16:21:34 -0700

    drills 02 completed

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,17 +1,48 @@
 
 
-function countOccurences(){
+function countOccurences(arr, val){
+    let count = 0;
+    for (v of arr) {
+        if (v === val) count++
+    }
+    return count;
 
 }
 
-function wordLengths(){
+function wordLengths(arr){
+    let arrLength = [];
+    for (v of arr) {
+        arrLength.push(v.length);
+    }
+    return arrLength;
+}
+
+function getMinMaxMean(arr){
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    const mean = arr.reduce((a,b) => a + b, 0 )/ arr.length;
+    const obj = {
+        "min": min,
+        "max": max,
+        "mean" : mean
+    };
+    return obj;
 
 }
 
-function getMinMaxMean(){
-
-}
-
-function findMode(){
+function findMode(arr){
+    const counter = [];
+    const numSet = [];
+    let index;
+    for (v of arr) {
+        index = numSet.indexOf(v);
+        if (index === -1) {
+            numSet.push(v);
+            counter.push(1);
+        } else {
+            counter[index]++;
+        }
+    }
+    return numSet[counter.lastIndexOf(Math.max(...counter))]
 
 }
