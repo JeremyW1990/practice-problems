b01ef39e45a4127ed79ec7dc9a8a33de79f59665
CJ Wang <shijie.wang1990@gmail.com>
2019-03-26 23:34:44 -0700

    drills 05 INCOMPLETE!

--- drills05/yourcode.js
+++ drills05/yourcode.js
@@ -1,7 +1,17 @@
 
 
-function populateRecords(){
-
+function populateRecords(objList){
+    let chargeAmount = 0;
+    let cashAdAmount = 0;
+    for (const obj of objList) 
+        obj['type'] === 'charge' ? chargeAmount += +obj['amount'] : cashAdAmount += +obj['amount'];
+        
+    
+    const result = {
+        "charge" : chargeAmount,
+        "cash advance" : cashAdAmount
+    }
+    return result;
 }
 
 
